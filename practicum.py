df6d8d2012d936bbe46a07ad52c0c4d1c1f026ce
KanoKito <kano-kito@yandex.ru>
2024-12-17 11:48:58 +0300

    linter fix

--- practicum.py
+++ practicum.py
@@ -5,19 +5,19 @@ def guess_the_number():
     # Определяем константы
     MIN_NUMBER = 1
     MAX_NUMBER = 100
-    
+
     # Получаем случайное число в заданном диапазоне
     number = randint(MIN_NUMBER, MAX_NUMBER)
     print(f'Угадайте число от {MIN_NUMBER} до {MAX_NUMBER}')
-    
+
     attempts = 0  # Счетчик попыток
-    
+
     while True:
         try:
             # Получаем число от пользователя и сохраняем его в переменную.
             guess = int(input('Введите число: '))
             attempts += 1  # Увеличиваем счетчик попыток
-            
+
             # Проверяем диапазон введенного числа
             if guess < MIN_NUMBER or guess > MAX_NUMBER:
                 print(
@@ -25,15 +25,15 @@ def guess_the_number():
                     f'{MIN_NUMBER} до {MAX_NUMBER}.'
                     )
                 continue
-            
+
             # Если число меньше загаданного...
             if guess < number:
                 print('Ваше число меньше того, что загадано.')
-                
+
             # Если число больше загаданного...
             elif guess > number:
                 print('Ваше число больше того, что загадано.')
-                
+
             # Если число угадано...
             else:
                 print(
@@ -41,7 +41,7 @@ def guess_the_number():
                     f'Вы угадали число за {attempts} попыток :)'
                     )
                 break
-                
+
         except ValueError:
             print('Пожалуйста, введите корректное целое число.')
 
